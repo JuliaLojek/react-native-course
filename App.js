8f1e14c4111c0742c220b67b3130eee6fd01e0b5
Julia Lojek <julialojek@gmail.com>
2020-06-24 14:50:18 +0200

    state

--- App.js
+++ App.js
@@ -1,16 +1,25 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
+  const [name, setName] = useState("Julia");
+  const [person, setPerson] = useState({ name: "Mario", age: 26 });
+
+  const clickHandler = () => {
+    setName(name === "Julia" ? "Fred" : "Julia");
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Text style={styles.title}>TITLE</Text>
       </View>
       <View style={styles.body}>
-        <Text>text text text text</Text>
-        <Text>text text text text</Text>
-        <Text>text text text text</Text>
+        <Text style={styles.text}>My name is {name}</Text>
+        <Text style={styles.text}>His name is {person.name} and his age is {person.age}</Text>
+        <View style={styles.button}>
+          <Button title="change my name!" color="#DD7777" onPress={clickHandler} />
+        </View>
       </View>
     </View>
   );
@@ -21,16 +30,23 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   header: {
-    backgroundColor: "pink",
+    backgroundColor: "#DD7777",
     padding: 20,
   },
   title: {
     fontWeight: "bold",
     fontSize: 24,
+    color: "#FFF",
   },
   body: {
-    backgroundColor: "yellow",
+    backgroundColor: "#9FCFBE",
     padding: 40,
     height: "100%",
-  }
+  },
+  text: {
+    fontSize: 20,
+  },
+  button: {
+    marginTop: 20,
+  },
 });
